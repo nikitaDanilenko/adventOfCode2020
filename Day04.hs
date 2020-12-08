fcca33e3a88a54663e6240904c42f946ff2a7588
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-12-08 22:36:03 +0100

    Add solutions for the first 5.5 days

---
+++ Day04.hs
@@ -0,0 +1,94 @@
+module Day04 where
+
+import Text.Parsec.String (Parser)
+import Text.Parsec.Char (char, anyChar, space, string, noneOf)
+import Text.Parsec (parse, many)
+import Data.List ( sort, isPrefixOf, isSuffixOf )
+import Data.Maybe (catMaybes)
+import Data.Char (isDigit, isHexDigit)
+
+data Passport = Passport { 
+    byr :: String, 
+    ecl :: String,
+    eyr :: String,
+    hcl :: String,
+    hgt :: String,
+    iyr :: String,
+    pid :: String
+  }
+  deriving Show
+
+passportParser :: Parser Passport
+passportParser = do
+    byrParam <- mkAttributeParser "byr"
+    space
+    eclParam <- mkAttributeParser "ecl"
+    space
+    eyrParam <- mkAttributeParser "eyr"
+    space
+    hclParam <- mkAttributeParser "hcl"
+    space
+    hgtParam <- mkAttributeParser "hgt"
+    space
+    iyrParam <- mkAttributeParser "iyr"
+    space
+    pidParam <- mkAttributeParser "pid"
+    return (Passport byrParam eclParam eyrParam hclParam hgtParam iyrParam pidParam)
+
+mkAttributeParser :: String -> Parser String
+mkAttributeParser name = do
+    string name
+    char ':'
+    many (noneOf " ")
+
+readPassport :: Parser Passport -> [String] -> Maybe Passport
+readPassport parser ls = 
+    case parse parser "" (unwords (filter (not . isPrefixOf "cid") (sort (words (unwords ls))))) of
+        Right p -> Just p
+        _ -> Nothing
+
+isValidPassport :: Passport -> Bool
+isValidPassport p = 
+    and [byrValid (byr p), eclValid (ecl p), eyrValid (eyr p), hclValid (hcl p), hgtValid (hgt p), iyrValid (iyr p), pidValid (pid p)]
+
+yearValid :: Int -> Int -> String -> Bool
+yearValid low high ys = all isDigit ys && low <= y && y <= high where
+    y = read ys :: Int
+
+byrValid :: String -> Bool
+byrValid = yearValid 1920 2002
+
+eclValid :: String -> Bool
+eclValid e = e `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
+eyrValid :: String -> Bool
+eyrValid = yearValid 2020 2030
+
+hclValid :: String -> Bool
+hclValid [] = False
+hclValid (c : cs) = c == '#' && length cs == 6 && all isHexDigit cs 
+
+hgtValid :: String -> Bool
+hgtValid s = validSuffixWith "cm" 150 193 s || validSuffixWith "in" 59 76 s where
+    validSuffixWith suffix low high s = isSuffixOf suffix s && low <= h && h <= high where
+      h =  read (takeWhile isDigit s) :: Int
+
+iyrValid :: String -> Bool
+iyrValid = yearValid 2010 2020
+
+pidValid :: String -> Bool
+pidValid s = all isDigit s && length s == 9
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn a = go where
+    go [] = []
+    go as = first : go (drop 1 last) where (first, last) = span (/= a) as
+
+readPassports :: Parser Passport -> String -> [Passport]
+readPassports p = catMaybes . map (readPassport p). splitOn "" . lines
+
+readAllValid :: (Passport -> Bool) -> IO Int
+readAllValid validator = do
+    text <- readFile "day04input.txt"
+    let valids = readPassports passportParser text
+    return (length (filter validator valids))
---
+++ Day05.hs
@@ -0,0 +1,44 @@
+data Position = Lower | Upper
+
+asNumber :: [Position] -> Integer
+asNumber ps = go 0 (2 ^ l - 1) ps where
+
+    l = length ps
+
+    go _ upper [] = upper
+    go lower upper (p : ps) = 
+        let halfInterval = (1 + upper - lower) `div` 2
+        in case p of
+            Lower -> go lower (upper - halfInterval) ps
+            Upper -> go (lower + halfInterval) upper ps
+
+readRow :: String -> Integer
+readRow = asNumber . map (asPosition 'F' 'B')
+
+readColumn :: String -> Integer
+readColumn = asNumber . map (asPosition 'L' 'R')
+
+readSeat :: String -> (Integer, Integer)
+readSeat s = (readRow front, readColumn back) where
+    front = take 7 s
+    back = drop 7 s
+
+idOfSeat :: Integer -> Integer -> Integer
+idOfSeat row col = 8 * row + col
+
+asPosition :: Char -> Char -> Char -> Position
+asPosition l u c 
+    | l == c = Lower 
+    | u == c = Upper
+
+solution :: IO Integer
+solution = do
+    text <- readFile "day05input.txt"
+    let ls = lines text
+        ids = map (uncurry idOfSeat . readSeat) ls
+    return (maximum ids)
+
+-- The first one is the base, so add 1 afterwards.
+findMissing :: [Integer] -> [Integer]
+findMissing xs = 
+    map fst (filter (\(x, y) -> y > 1 + x) (zip xs (tail xs)))
