473d5da79e9dd5d3e0f3849a842f4786ec43ef88
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-12-08 22:41:37 +0100

    Rearrange files

---
+++ solutions/Day01.hs
@@ -0,0 +1,11 @@
+allFitting2 :: Int -> [Int] -> [((Int, Int), Int)]
+allFitting2 target vs = [((x, y), x * y) | x <- vs, y <- vs, x + y == target]
+
+allFitting3 :: Int -> [Int] -> [((Int, Int, Int), Int)]
+allFitting3 target vs = [((x, y, z), x * y * z) | x <- vs, y <- vs, z <- vs, x + y + z == target]
+
+readValues :: IO [Int]
+readValues = do
+    text <- readFile "./inputs/day01.txt"
+    let ls = lines text
+    return (map read ls)
---
+++ solutions/Day06.hs
@@ -0,0 +1,19 @@
+module Day06 where
+
+import Day04 ( splitOn )
+import Data.Set ( Set, fromList, intersection, size )
+
+countGroup :: [String] -> Int
+countGroup = size . fromList . concat
+
+countGroups :: [[String]] -> Int
+countGroups = sum . map countGroup
+
+solution1 :: IO Int
+solution1 = do
+    text <- readFile "./inputs/day06.txt"
+    let gs = splitOn "" (lines text)
+    return (countGroups gs)
+
+countGroup2 :: [String] -> Int
+countGroup2 = size . foldr1 intersection . map fromList
