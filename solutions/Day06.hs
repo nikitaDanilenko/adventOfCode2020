8efade4cbb493c8075304c13d08e8ec4cf8e1377
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-12-08 22:47:36 +0100

    Add solution for second part of day 6

--- solutions/Day06.hs
+++ solutions/Day06.hs
@@ -6,14 +6,21 @@ import Data.Set ( Set, fromList, intersection, size )
 countGroup :: [String] -> Int
 countGroup = size . fromList . concat
 
-countGroups :: [[String]] -> Int
-countGroups = sum . map countGroup
-
-solution1 :: IO Int
-solution1 = do
-    text <- readFile "./inputs/day06.txt"
-    let gs = splitOn "" (lines text)
-    return (countGroups gs)
-
 countGroup2 :: [String] -> Int
 countGroup2 = size . foldr1 intersection . map fromList
+
+countGroupsWith :: ([String] -> Int) -> [[String]] -> Int
+countGroupsWith cg = sum . map cg
+
+solutionWith :: ([String] -> Int) -> IO Int
+solutionWith cg = do
+    text <- readFile "../inputs/day06.txt"
+    let gs = splitOn "" (lines text)
+    return (countGroupsWith cg gs)
+
+
+solution1 :: IO Int
+solution1 = solutionWith countGroup
+
+solution2 :: IO Int
+solution2 = solutionWith countGroup2
