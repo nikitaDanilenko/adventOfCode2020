7ea818e3d4b353da1a96edd993fa937ac5b2c796
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-12-12 04:36:27 +0100

    Add day 11 solution for second part

--- solutions/Day11.hs
+++ solutions/Day11.hs
@@ -1,9 +1,9 @@
 module Day11 where
 
-import Data.Map ( Map, fromList, toList, mapWithKey, size )
+import Data.Map ( Map, fromList, toList, mapWithKey, size, keys, (!) )
 import qualified Data.Map as M ( lookup, filter )
 import Data.Maybe ( catMaybes )
-import Data.List (sortBy, groupBy)
+import Data.List (sortBy, groupBy, sort, find, minimumBy)
 import Data.Ord (comparing)
 import Crypto.Hash (Digest, hash)
 import Crypto.Hash.Algorithms (SHA512)
@@ -20,6 +20,10 @@ isEmpty _ = False
 isTaken :: Pos -> Bool
 isTaken Taken = True
 isTaken _ = False
+
+isSeat :: Pos -> Bool
+isSeat Floor = False
+isSeat _ = True
 
 floorSymbol, emptySymbol, takenSymbol :: Char
 floorSymbol = '.'
@@ -45,33 +49,65 @@ mkMap = fromList . concat . zipWith (\i ps -> zipWith (\j p -> ((i, j), p)) [0 .
 offsetPairs :: [(Int, Int)]
 offsetPairs = [(a, b) | a <- [-1, 0, 1], b <- [-1, 0, 1], a /= b || a /= 0]
 
+-- The neighbours should be computed only once, but since adding offsets is very cheap, the below implementation is fine.
 neighbours :: SeatMap -> (Int, Int) -> [Pos]
 neighbours sm (i, j) = catMaybes [M.lookup (i + a, j + b) sm | (a, b) <- offsetPairs]
 
-applyRules :: Pos -> [Pos] -> Pos
-applyRules p ns
+type Visible = Map (Int, Int) [(Int, Int)]
+
+mkVisible :: SeatMap -> Visible
+mkVisible sm = mapWithKey (\p _ -> visibleFor p) sm
+  where
+    smIndices = keys sm
+    visibleFor (i, j) = catMaybes (map (firstDefinedWith sm) [ups, downs, lefts, rights, topLefts, topRights, bottomLefts, bottomRights]) where
+      ups = filter (\(a, b) -> a < i && b == j) smIndices
+      downs = filter (\(a, b) -> a > i && b == j) smIndices
+      lefts = filter (\(a, b) -> a == i && b < j) smIndices
+      rights = filter (\(a, b) -> a == i && b > j) smIndices
+      topLefts = filter (\(a, b) -> a < i && b < j && b - a == j - i) smIndices
+      topRights = filter (\(a, b) -> a < i && b > j && i - a == b - j) smIndices
+      bottomLefts = filter (\(a, b) -> a > i && b < j && a - i == j - b) smIndices
+      bottomRights = filter (\(a, b) -> a > i && b > j && b - a == j - i) smIndices
+
+      distanceTo (a, b) = abs (a - i) + abs (b - j)
+
+      firstDefinedWith sm inds =
+        let candidates = filter (\p -> isSeat (sm ! p)) inds
+        in if null candidates then Nothing else Just (minimumBy (comparing distanceTo) candidates)
+
+neighbours2 :: Visible -> SeatMap -> (Int, Int) -> [Pos]
+neighbours2 vs sm (i, j) = map (sm !) (vs ! (i, j))
+
+applyRules :: Int -> Pos -> [Pos] -> Pos
+applyRules allowed p ns
   | isEmpty p && all (not . isTaken) ns = Taken
-  | isTaken p && length (filter isTaken ns) >= 4 = Empty
+  | isTaken p && length (filter isTaken ns) >= allowed = Empty
   | otherwise = p
 
-applyRulesAll :: SeatMap -> SeatMap
-applyRulesAll sm =
-  mapWithKey (\(i, j) p -> applyRules p (neighbours sm (i, j))) sm
+applyRulesAll :: Int -> (SeatMap -> (Int, Int) -> [Pos]) -> SeatMap -> SeatMap
+applyRulesAll allowed nf sm =
+  mapWithKey (\(i, j) p -> applyRules allowed p (nf sm (i, j))) (M.filter isSeat sm)
 
-step :: SeatMap -> Set String -> (SeatMap, Set String)
-step sm seen = (applyRulesAll sm, hashOf sm `insert` seen)
+step :: Int -> (SeatMap -> (Int, Int) -> [Pos]) -> SeatMap -> Set String -> (SeatMap, Set String)
+step a nf sm seen = (applyRulesAll a nf sm, hashOf sm `insert` seen)
 
-fixpointSize :: SeatMap -> Int
-fixpointSize sm =
+fixpointSize :: Int -> (SeatMap -> (Int, Int) -> [Pos]) -> SeatMap -> Int
+fixpointSize a nf sm =
   size
     $ M.filter isTaken
     $ fst
     $ head
     $ dropWhile (\(sm, set) -> not (hashOf sm `member` set))
-    $ iterate (uncurry step) (sm, empty)
+    $ iterate (uncurry (step a nf)) (sm, empty)
 
 readSeatMap :: IO SeatMap
 readSeatMap = fmap mkMap (readFile "inputs/day11.txt")
+
+solution1 :: IO Int
+solution1 = fmap (fixpointSize 4 neighbours) readSeatMap
+
+solution2 :: IO Int
+solution2 = fmap (\sm -> fixpointSize 5 (neighbours2 (mkVisible sm)) sm) readSeatMap
 
 type Hashed = Digest SHA512
 
